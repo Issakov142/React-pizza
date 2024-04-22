9323254ecfcb9431ca6dbf50c226b011b2caabb2
Issakov142 <82026984+Issakov142@users.noreply.github.com>
2024-04-22 17:25:48 +0500

    new page home jsx

--- src/App.js
+++ src/App.js
@@ -3,40 +3,15 @@ import React from 'react';
 import './scss/app.scss';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaCard';
-import PizzaSkeleton from './components/PizzaCard/Skeleton';
+import Home from './pages/Home';
 
 function App() {
-  const [pizzas, setPizzas] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  React.useEffect(() => {
-    fetch('https://661ed28b16358961cd92e725.mockapi.io/pizzas')
-      .then((res) => res.json())
-      .then((data) => {
-        setPizzas(data);
-        setIsLoading(false);
-      });
-  }, []);
-  console.log(pizzas);
-
   return (
     <div className="wrapper">
       <Header />
-
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
-              : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <Home />
         </div>
       </div>
     </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaCard';
+import PizzaSkeleton from '../components/PizzaCard/Skeleton';
+
+const Home = () => {
+  const [pizzas, setPizzas] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  React.useEffect(() => {
+    fetch('https://661ed28b16358961cd92e725.mockapi.io/pizzas')
+      .then((res) => res.json())
+      .then((data) => {
+        setPizzas(data);
+        setIsLoading(false);
+      });
+  }, []);
+  console.log(pizzas);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
+          : pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
+export default Home;
